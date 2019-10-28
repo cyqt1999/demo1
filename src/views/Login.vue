26e793f120dbbef76927dbee31cbf6be6a6e8775
JavaScript <3284885524@qq.com>
2019-10-28 17:14:07 +0800

    登录验证

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,21 +10,23 @@
       </div>
       <div class="dialog-box-center"></div>
       <div class="dialog-box-right">
-        <el-form
-          :model="ruleForm"
-          status-icon
-          :rules="rules"
-          ref="ruleForm"
-          label-width="100px"
-          class="demo-ruleForm"
-        >
-          <el-input v-model="ruleForm.userName" prefix-icon="el-icon-s-custom" placeholder="请输入用户名"></el-input>
-          <el-input
-            placeholder="请输入密码"
-            prefix-icon="el-icon-lock"
-            v-model="ruleForm.userPass"
-            show-password
-          ></el-input>
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" >
+          <el-form-item prop="userName">
+            <el-input
+              type="text"
+              v-model.trim="ruleForm.userName"
+              prefix-icon="el-icon-s-custom"
+              placeholder="请输入用户名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="userPass">
+            <el-input
+              placeholder="请输入密码"
+              prefix-icon="el-icon-lock"
+              v-model.trim="ruleForm.userPass"
+              show-password
+            ></el-input>
+          </el-form-item>
           <el-checkbox v-model="checked">记住密码</el-checkbox>
           <el-button type="success" :loading="loding" @click="submitForm('ruleForm')">登录</el-button>
         </el-form>
@@ -38,33 +40,21 @@
 export default {
   name: "login",
   data() {
-    var checkUserName = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("用户名不能为空"));
-      }
-    };
-    var validatePass = (rule, value, callback) => {
-      if (value.trim() === "") {
-        callback(new Error("密码不能为空"));
-      } else {
-        if (this.ruleForm.checkPass !== "") {
-          this.$refs.ruleForm.validateField("checkPass");
-        }
-        callback();
-      }
-    };
     return {
       //表单数据
       ruleForm: {
         userName: "",
         userPass: ""
       },
-
       rules: {
-        //表单验证
-        userName: [{ validator: checkUserName, trigger: "blur" }],
-        userPass: [{ validator: validatePass, trigger: "blur" }]
-      },
+          userName: [
+            { required: true, message: '用户名不能为空', trigger: 'blur' }
+          ],
+          userPass: [
+            { required: true, message: '密码不能为空', trigger: 'blur' },
+             { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          ]
+        },
       checked: false,
       loding: false
     };
@@ -75,9 +65,17 @@ export default {
      * @param {String} formName获取用户提交的数据
      * **/
     submitForm(formName) {
-      console.log(formName);
       const _this = this;
       _this.loding = true;
+      _this.$refs[formName].validate((valid) => {
+          if (!valid) {
+            _this.$message.error('用户名或密码错误！');
+            return false;
+          } else {
+            _this.$message.success('登录成功！');
+          }
+        });
+      _this.loding = false;
     }
   }
 };
@@ -85,7 +83,7 @@ export default {
 <style lang="less" scoped>
 @numMax: 100%;
 @fifty: 50%;
-@borderNumWidth:85%;
+@borderNumWidth: 85%;
 .login {
   background-image: url(../assets/background.jpg);
   background-repeat: no-repeat;
@@ -131,10 +129,13 @@ export default {
       height: @numMax;
       width: @fifty;
       margin-top: 40px;
-      // 输入框宽度 
-      .el-input {
-        width: @borderNumWidth;
-        margin-bottom: 30px;
+      // 输入框宽度
+
+      .el-form-item {
+        margin-bottom: 40px;
+        .el-input {
+          width: @borderNumWidth;
+        }
       }
       // 输入框 边框颜色，去掉圆角
       /deep/.el-input--prefix .el-input__inner {
@@ -145,14 +146,17 @@ export default {
         width: @borderNumWidth;
         margin-top: 30px;
       }
-        /deep/.el-input__icon {
-          color: #24a33a;
-          font-size: 20px;
-        }
-        /deep/.el-icon-view:before{
-          font-size: 16px;
-          color: #909399;
-        }
+      /deep/.el-input__icon {
+        color: #24a33a;
+        font-size: 20px;
+      }
+      /deep/.el-icon-view:before {
+        font-size: 16px;
+        color: #909399;
+      }
+      /deep/.el-form-item__error {
+        left: 25px;
+      }
     }
   }
 }
