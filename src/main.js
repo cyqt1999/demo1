9733dd5f029c05fe1665850878fd260a2d1b10f1
JavaScript <3284885524@qq.com>
2019-10-28 14:17:59 +0800

    4546

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import './assets/iconfont/iconfont.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
