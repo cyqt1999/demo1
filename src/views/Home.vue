1f84ed33fbc0f07aa3b90ad21498b8d2d2fc0813
JavaScript <3284885524@qq.com>
2019-10-30 09:02:48 +0800

    mock 1.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,28 +5,15 @@
   </div>
 </template>
 <script>
-import Mock from 'mockjs' //node方式引入（CommonJS）
-import request from '@/http/request'
-
 
 export default {
   name:"home",
   data (){
     return {
-      data:[]//生成数据
     }
   },
   methods:{
   },
-  mounted(){
-    const _this = this;
-    _this.axios.get('/api/data').then(res => {//get()中参数要要mock.js文件中的Mock.mock()配置的路由保持一致
-      _this.data = res.data.data;
-      console.log(res.data);//在console中看到数据
-    }).catch(res => {
-      alert('wrong');
-    })
-  }
 }
 </script>
 <style lang="less" scoped>
